47e28c34011fe213a24f0581dba8159ed82341d7
sotheanynoun <sotheanynoun@gmail.com>
2025-02-11 15:03:53 +0700

    admin backend category option setup

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ClientController;
@@ -67,3 +68,20 @@ Route::get('/client/register', [ClientController::class,'ClientRegister'])->name
 Route::post('/client/register/submit', [ClientController::class,'ClientRegisterSubmit'])->name('client.register.submit');
 Route::post('/client/login_submit', [ClientController::class,'ClientLoginSubmit'])->name('client.login_submit');
 Route::get('/client/logout', [ClientController::class,'ClientLogout'])->name('client.logout');
+
+
+//All Admin Category
+Route::middleware('admin')->group(function(){
+    Route::controller(CategoryController::class)->group(function(){
+        Route::get('/all/category','AllCategory')->name('all.category');
+        Route::get('/add/category','AddCategory')->name('add.category');
+        Route::post('/category/store','CategoryStore')->name('category.store');
+        Route::get('/edit/category/{id}','EditCategory')->name('edit.category');
+        Route::post('/category/update','CategoryUpdate')->name('category.update');
+        Route::get('/delete/category/{id}','DeleteCategory')->name('delete.category');
+
+    });
+    
+});
+
+//End Admin Middleware
