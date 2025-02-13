e44e5d85ea79bf1d1316888c826d1d8492235a4d
sotheanynoun <sotheanynoun@gmail.com>
2025-02-13 14:03:12 +0700

    restaurant backend product option setup

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -107,7 +107,7 @@
 
           <div class="card sidebar-alert border-0 text-center mx-4 mb-0 mt-5">
               <div class="card-body">
-                  <img src="assets/images/giftbox.png" alt="">
+                  <img src="{{ asset('backend/assets/images/giftbox.png') }}" alt="">
                   <div class="mt-4">
                       <h5 class="alertcard-title font-size-16">Unlimited Access</h5>
                       <p class="font-size-13">Upgrade your plan from a Free trial, to select ‘Business Plan’.</p>
